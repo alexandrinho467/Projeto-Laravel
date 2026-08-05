d3ace9bc655e15f98dcc61296bbdf3e122f7acbe
alexandrinho467 <alediasdemello77@gmail.com>
2026-08-05 15:35:32 -0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,7 @@ class DatabaseSeeder extends Seeder
             'site_email'       => 'suporte@alexandredias.com.br',
             'banner_title'     => 'Nova Coleção 2025',
             'banner_subtitle'  => 'Até 50% Off',
-            'banner_img'       => '/assets/images/banner-principal-2.jpg',
+            'banner_img'       => 'http://127.0.0.1:8000/assets/images/banner-principal-2.jpg',
             'free_shipping_min'=> '1500',
         ];
         foreach ($settings as $k => $v) SiteSetting::set($k, $v);
